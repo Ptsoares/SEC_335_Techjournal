4c4d02ad3f2c16ed6edb8e23905f99419937ea92
Parker Soares <71037439+Ptsoares@users.noreply.github.com>
2023-01-28 16:58:45 -0500

    Update portscanner.sh
    Added user prompt in place of assumed addition of files after bash file

--- portscanner.sh
+++ portscanner.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-hostfile=$1
-portfile=$2
-echo "host,port"
-for host in $(cat $hostfile); do
-  for port in $(cat $portfile); do
+# This is a modified bash script provided in the ethical hacking and pen. testing course SEC-335.
+# Outside reference: https://linuxhint.com/prompt-for-input-bash/
+
+read -e -p "Enter the hosts (target) file:" HOSTFILE
+
+read -e -p "Enter the ports (tcp/udp) file:" PORTFILE
+
+echo "host:port"
+for host in $(cat $HOSTFILE); do
+  for port in $(cat $PORTFILE); do
     timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
       echo "$host:$port"
   done
