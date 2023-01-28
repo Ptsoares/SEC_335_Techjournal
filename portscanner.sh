3ebf41674c08e25153177d5c7b6b49ea1305dd15
Parker Soares <71037439+Ptsoares@users.noreply.github.com>
2023-01-28 16:41:31 -0500

    Update portscanner.sh to format with colon

--- portscanner.sh
+++ portscanner.sh
@@ -6,6 +6,6 @@ echo "host,port"
 for host in $(cat $hostfile); do
   for port in $(cat $portfile); do
     timeout .1 bash -c "echo >/dev/tcp/$host/$port" 2>/dev/null &&
-      echo "$host,$port"
+      echo "$host:$port"
   done
 done
