b59730fd5fb9e2b19396cc60783316a0b652ff6b
Parker Soares <71037439+Ptsoares@users.noreply.github.com>
2023-02-05 12:32:38 -0500

    Create DNSlookup.sh

---
+++ Week3/DNSlookup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# This is a bash script for the ethical hacking and pen. testing course SEC-335. When you provide a network prefix and a DNS server, the script will return the hostnames for the valid servers.
+
+prefix=$1
+dns=$2
+
+for i in $(seq 1 254); do
+    timeout .1 bash -c "echo nslookup $prefix.$i $dns" 2>/dev/null
+done
